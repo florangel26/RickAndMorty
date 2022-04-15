a02024f48e991b8812ce13d42aa5370c4cd460b1
Florangel Orochena <flory26991@gmail.com>
2022-04-15 19:10:41 -0400

    'main'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="titulo">
+      <h1 class="h1">Rick and Morty</h1>
+       </div>
+   
+        <PersonajesSerie v-for="(character, index) in characters" :key="index" :datos="character"/>
+          
+           </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PersonajesSerie from './components/PersonajesSerie.vue'
 
 export default {
-  name: "App",
+  name: 'app',
   components: {
-    HelloWorld,
+    PersonajesSerie,
   },
-};
+  data() {
+    return {
+      characters: [
+        {
+          nombre: 'Rick Sanchez',
+          src: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        },
+        {
+          nombre: 'Morty Smith',
+          src: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        },
+        {
+          nombre: 'Summer Smith',
+          src: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+        },
+        {
+          nombre: 'Beth Smith',
+          src: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+        },
+        {
+          nombre: 'Jerry Smith',
+          src: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+        },
+        {
+          nombre: 'Abadango Cluster Princess',
+          src: 'https://rickandmortyapi.com/api/character/avatar/6.jpeg',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style>
+body{
+ background-image: url('https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Shadows Into Light', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  color: #03f153;
 }
+.titulo{
+ padding-top: 50px;
+ text-align: center;
+}
+.h1{
+ font-size: 70px;
+
+
+}
+
 </style>
---
+++ src/components/PersonajesSerie.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="pipol">
+      <img :src="datos.src" :alt="datos.nombre" class="imagen" />
+      <p>{{ datos.nombre }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    datos: {
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+ .pipol{
+   width: 100%;
+   padding: 40px;
+   font-size: 40px;
+   
+ }
+
+  
+.imagen {
+  border-radius: 50%;
+  position: relative;
+}
+</style>
